7bd4649325c1832d147a95f488dcb379074d22df
GucioPL123 <166158045+GucioPL123@users.noreply.github.com>
2024-10-28 20:32:49 +0100

    zazbrajanie

--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    xD = false
+})
 function pin () {
     music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
     music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
 }
+input.onButtonPressed(Button.B, function () {
+    xD = true
+})
+let xD = false
 pins.digitalWritePin(DigitalPin.P0, 1)
 music.setVolume(255)
 basic.forever(function () {
     if (input.pinIsPressed(TouchPin.P0)) {
-        serial.writeValue("x", 0)
-    } else {
+        music.stopAllSounds()
+    } else if (xD == false) {
         serial.writeValue("x", 1)
         pin()
-        radio.sendValue("ALA", 1)
+        radio.sendNumber(1)
     }
 })
