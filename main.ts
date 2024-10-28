142bc20e149dcd9c8f9b5b5bb082b19aa94c8c42
GucioPL123 <166158045+GucioPL123@users.noreply.github.com>
2024-10-28 20:37:43 +0100

    delete USB code

--- main.ts
+++ main.ts
@@ -1,21 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    xD = false
+    Sounds_ONOFF = false
 })
 function pin () {
     music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
     music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
 }
 input.onButtonPressed(Button.B, function () {
-    xD = true
+    Sounds_ONOFF = true
 })
-let xD = false
+let Sounds_ONOFF = false
 pins.digitalWritePin(DigitalPin.P0, 1)
 music.setVolume(255)
 basic.forever(function () {
     if (input.pinIsPressed(TouchPin.P0)) {
         music.stopAllSounds()
-    } else if (xD == false) {
-        serial.writeValue("x", 1)
+    } else if (Sounds_ONOFF == false) {
         pin()
         radio.sendNumber(1)
     }
